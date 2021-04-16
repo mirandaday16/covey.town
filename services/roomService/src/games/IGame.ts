d11d2a41cd47d7797f6080cf767bdf95d5c389ec
Bayley Carter <carter.ba@northeastern.edu>
2021-04-16 13:51:04 -0400

    removed unused methods

--- services/roomService/src/games/IGame.ts
+++ services/roomService/src/games/IGame.ts
@@ -19,9 +19,5 @@ export default interface IGame {
 
   move(move: TTLPlayer1Move | TTLPlayer2Move | HangmanPlayer1Move | HangmanPlayer2Move) : void;
 
-  isGameOver() : boolean;
-
-  finishGame(winningPlayerID: string): string;
-
   playerJoin(player2ID: string, player2Username: string) : void;
 }
